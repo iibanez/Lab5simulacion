7f3a165031d69efe54ed37ce93b2b6979386b311
Ignacio Ibañez <ignacio.ibanez@usach.cl>
2017-06-28 20:22:05 -0400

    agregado getopt

--- OO/buildmc.cc
+++ OO/buildmc.cc
@@ -1,11 +1,42 @@
 #include "CadenaMarkov.hh"
 
-int main(){
+#include <unistd.h>
+
+int main(int argc,char*argv[]){
     
-    int numerodeestados = 4;
-    std::string archivodeentrada = "dna.txt";
-    std::string archivodesalida = "cadenademarkov.txt";
-    
+    int numerodeestados;
+    std::string archivodeentrada;
+    std::string archivodesalida;
+
+    opterr = 0;
+    int c;
+
+    while ((c = getopt (argc, argv, ":i:N:o:")) != -1){
+        switch (c)
+          {
+            case 'i':
+                archivodeentrada = optarg;
+                break;
+            case 'N':
+                numerodeestados = atoi(optarg);
+                break;
+            case 'o':
+                archivodesalida = optarg;
+                break;
+            case ':':
+                fprintf(stderr, "%s: option '-%c' requires an argument\n",argv[0], optopt);
+                return 1;
+            case '?':
+                if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr, "Unknown option character `\\x%c'.\n", optopt);
+                return 1;
+            default:
+                abort ();
+          }
+    }
+
     //inicializar la cadena de markov
     CadenaMarkov cm(numerodeestados, archivodeentrada, archivodesalida);
     
--- OO/pimc.cc
+++ OO/pimc.cc
@@ -1,11 +1,38 @@
 #include "MatrizEstacionaria.hh"
 
+#include <unistd.h>
 
-int main(){
+int main(int argc,char*argv[]){
     
-    std::string archivodeentrada = "cadenademarkov.txt";
-    std::string archivodesalida = "cadenaestacionaria.txt";
+    std::string archivodeentrada;
+    std::string archivodesalida;
     
+    opterr = 0;
+    int c;
+
+    while ((c = getopt (argc, argv, ":i:o:")) != -1){
+        switch (c)
+          {
+            case 'i':
+                archivodeentrada = optarg;
+                break;
+            case 'o':
+                archivodesalida = optarg;
+                break;
+            case ':':
+                fprintf(stderr, "%s: option '-%c' requires an argument\n",argv[0], optopt);
+                return 1;
+            case '?':
+                if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr, "Unknown option character `\\x%c'.\n", optopt);
+                return 1;
+            default:
+                abort ();
+          }
+    }
+
     MatrizEstacionaria me(archivodeentrada, archivodesalida);
 
     me.lecturaArchivo();
